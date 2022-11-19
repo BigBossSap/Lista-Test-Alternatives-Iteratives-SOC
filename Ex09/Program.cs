b01275c3201670fede4cb4ccc346674117e5d03c
Sergi Alesanco Perich <sergi.alesanco.perich@gmail.com>
2022-11-19 18:18:04 +0100

    lol

---
+++ Ex09/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ex09
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*9. Escriure un programa en ‘C’ que ens proporcioni la següent sortida:
+   1 = 1
+   1 + 2 = 3
+   1 + 2 + 3 = 6 */
+
+
+            int acum = 0, i=1,k=0,suma=0;
+
+            while (i <= num1 / 2)
+            {
+                if (num1 % i == 0)
+                    sumadivisors += i;
+                i++;
+
+            }
+            
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
